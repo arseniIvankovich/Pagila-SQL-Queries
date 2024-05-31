f3675062e6a6f82343bcef46e0f129df18b0f889
arseniIvankovich <arseniivankovich0@gmail.com>
2024-05-31 12:30:47 +0200

    Refactor code

--- spark.py
+++ spark.py
@@ -19,9 +19,11 @@ def spark_session() -> SparkSession:
     Return spark session
     """
     
-    spark = SparkSession.builder \
-    .config("spark.jars", "/opt/spark/jars/postgresql-42.7.3.jar")\
-    .appName("PySpark_Postgres").getOrCreate()
+    spark = (
+        SparkSession.builder 
+        .config("spark.jars", "/opt/spark/jars/postgresql-42.7.3.jar")
+        .appName("PySpark_Postgres").getOrCreate()
+    )
 
     return spark
 
@@ -41,12 +43,19 @@ def film_and_category(category_df: pdf.DataFrame, film_category_df: pdf.DataFram
         pyspark.sql.dataframe.DataFrame:  result dataframe
     """
     
-    joined_df = category_df.join(film_category_df, category_df.category_id == film_category_df.category_id,'left') \
-                        .join(film_df, film_category_df.film_id == film_df.film_id, 'left')
+    joined_df = (
+            category_df
+            .join(film_category_df, category_df.category_id == film_category_df.category_id,'left')
+            .join(film_df, film_category_df.film_id == film_df.film_id, 'left')
+    )
 
-    result_df = joined_df.groupBy(category_df.name) \
-                        .agg(count(film_df.film_id).alias('number_of_films')) \
-                        .orderBy(col('number_of_films').desc())
+    result_df = (
+        joined_df
+        .groupBy(category_df.name) 
+        .agg(count(film_df.film_id).alias('number_of_films'))
+        .orderBy(col('number_of_films').desc())
+    )
+    
                             
     return result_df
 
@@ -68,14 +77,21 @@ def ten_actors_most_rented_count(actor_df: pdf.DataFrame, film_actor_df: pdf.Dat
         pyspark.sql.dataframe.DataFrame:  result dataframe
     """
     
-    joined_df = actor_df.join(film_actor_df, actor_df.actor_id == film_actor_df.actor_id)\
-                        .join(film_df, film_actor_df.film_id == film_df.film_id)\
-                        .join(inventory_df, film_df.film_id == inventory_df.film_id)\
-                        .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)
+    joined_df = (
+            actor_df.join(film_actor_df, actor_df.actor_id == film_actor_df.actor_id)
+            .join(film_df, film_actor_df.film_id == film_df.film_id)
+            .join(inventory_df, film_df.film_id == inventory_df.film_id)
+            .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)
+    )
     
-    result_df = joined_df.groupBy(actor_df.first_name, actor_df.last_name, actor_df.actor_id)\
-                        .agg(count(rental_df.rental_id).alias('rental_count'))\
-                        .orderBy(col('rental_count').desc())
+    
+    result_df = (
+        joined_df
+        .groupBy(actor_df.first_name, actor_df.last_name, actor_df.actor_id)
+        .agg(count(rental_df.rental_id).alias('rental_count'))
+        .orderBy(col('rental_count').desc())
+    )
+    
                         
     return result_df.select("first_name", "last_name", "rental_count").limit(10)
     
@@ -98,17 +114,23 @@ def category_most_spend_money(category_df: pdf.DataFrame, film_category_df: pdf.
         pyspark.sql.dataframe.DataFrame:  result dataframe
     """
     
-    joined_df = category_df.join(film_category_df, category_df.category_id == film_category_df.category_id)\
-                        .join(film_df, film_category_df.film_id == film_df.film_id)\
-                        .join(inventory_df, film_df.film_id == inventory_df.film_id)\
-                        .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)\
-                        .join(payment_df, payment_df.rental_id == rental_df.rental_id)
+    joined_df = (
+        category_df
+        .join(film_category_df, category_df.category_id == film_category_df.category_id)
+        .join(film_df, film_category_df.film_id == film_df.film_id)
+        .join(inventory_df, film_df.film_id == inventory_df.film_id)
+        .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)
+        .join(payment_df, payment_df.rental_id == rental_df.rental_id)
+    )
     
     
-    result_df = joined_df.groupBy(category_df.category_id, category_df.name)\
-                            .agg(sum(payment_df.amount).alias('sum_spend'))\
-                            .orderBy(col('sum_spend').desc())
-
+    result_df = (
+        joined_df
+        .groupBy(category_df.category_id, category_df.name)
+        .agg(sum(payment_df.amount).alias('sum_spend'))
+        .orderBy(col('sum_spend').desc()
+    )
+)
     return result_df.select(category_df.name, 'sum_spend').limit(1)
 
 def film_not_in_inventory(film_df: pdf.DataFrame, inventory_df: pdf.DataFrame) -> pdf.DataFrame:
@@ -122,7 +144,10 @@ def film_not_in_inventory(film_df: pdf.DataFrame, inventory_df: pdf.DataFrame) -
         pyspark.sql.dataframe.DataFrame:  result dataframe
     """
     
-    result_df = film_df.join(inventory_df, film_df.film_id == inventory_df.film_id,'left_anti')
+    result_df = (
+            film_df
+            .join(inventory_df, film_df.film_id == inventory_df.film_id,'left_anti')
+    )
     
     return result_df
 
@@ -144,25 +169,35 @@ def top_3_actor_chikdren_category(category_df: pdf.DataFrame, film_category_df: 
         pyspark.sql.dataframe.DataFrame:  result dataframe
     """
     
-    joined_df = category_df \
-                        .join(film_category_df, category_df.category_id == film_category_df.category_id) \
-                        .join(film_df, film_category_df.film_id == film_df.film_id) \
-                        .join(film_actor_df, film_df.film_id == film_actor_df.film_id) \
-                        .join(actor_df, film_actor_df.actor_id == actor_df.actor_id)
+    joined_df = (
+        category_df
+        .join(film_category_df, category_df.category_id == film_category_df.category_id)
+        .join(film_df, film_category_df.film_id == film_df.film_id)
+        .join(film_actor_df, film_df.film_id == film_actor_df.film_id)
+        .join(actor_df, film_actor_df.actor_id == actor_df.actor_id)
+    )
+    
                         
     children_df = joined_df.filter(category_df.name == 'Children')
 
-    actor_film_count_df = children_df.groupBy(actor_df.actor_id) \
+    actor_film_count_df = (
+        children_df
+        .groupBy(actor_df.actor_id)
         .agg(count(film_df.film_id).alias('film_count'))
+    )
+    
 
     window = Window.partitionBy(lit(0)).orderBy(col('film_count').desc())
     ranked_df = actor_film_count_df.withColumn('row_rank', dense_rank().over(window))
 
     top_ranked_df = ranked_df.filter(col('row_rank').isin(1, 2, 3))
 
-    result_df = top_ranked_df.join(actor_df, top_ranked_df.actor_id == actor_df.actor_id) \
+    result_df = (
+        top_ranked_df
+        .join(actor_df, top_ranked_df.actor_id == actor_df.actor_id)
         .select('first_name', 'last_name')
-
+    )
+    
     return result_df
 
 def city_with_active_inactive_customers(city_df: pdf.DataFrame, address_df: pdf.DataFrame, 
@@ -179,9 +214,12 @@ def city_with_active_inactive_customers(city_df: pdf.DataFrame, address_df: pdf.
         pyspark.sql.dataframe.DataFrame: result dataframe
     """
     
-    joined_df = city_df \
-                    .join(address_df, city_df.city_id == address_df.city_id) \
-                    .join(customer_df, address_df.address_id == customer_df.address_id)
+    joined_df = (
+        city_df 
+        .join(address_df, city_df.city_id == address_df.city_id)
+        .join(customer_df, address_df.address_id == customer_df.address_id)
+    )
+    
 
     window_spec = Window.partitionBy(city_df.city_id)
 
@@ -193,9 +231,13 @@ def city_with_active_inactive_customers(city_df: pdf.DataFrame, address_df: pdf.
         sum(when(customer_df.active == 0, 1).otherwise(0)).over(window_spec)
     )
 
-    result_df = joined_df.select(city_df.city,col("active_customer"),
-                                 col("inactive_customer"))\
-    .distinct().orderBy(col("inactive_customer").desc())
+    result_df = (
+        joined_df
+        .select(city_df.city,col("active_customer"), col("inactive_customer"))
+        .distinct()
+        .orderBy(col("inactive_customer").desc())
+    )
+    
     
     return result_df
 
@@ -222,27 +264,41 @@ def city_with_highest_rent(category_df: pdf.DataFrame, film_category_df: pdf.Dat
         pyspark.sql.dataframe.DataFrame: result dataframe
     """
     
-    joined_df = category_df \
-                .join(film_category_df, category_df.category_id == film_category_df.category_id)\
-                .join(film_df, film_category_df.film_id == film_df.film_id)\
-                .join(inventory_df, film_df.film_id == inventory_df.film_id)\
-                .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)\
-                .join(customer_df, rental_df.customer_id == customer_df.customer_id) \
-                .join(address_df, customer_df.address_id == address_df.address_id) \
-                .join(city_df, address_df.city_id == city_df.city_id)
-    
-    category_rent_df = joined_df.groupBy(category_df.name) \
-    .agg(
-        sum(when(city_df.city.like("a%"), film_df.rental_duration).otherwise(0)).alias("rent_a"),
-        sum(when(city_df.city.like("%-%"), film_df.rental_duration).otherwise(0)).alias("rent_")
+    joined_df = (
+        category_df 
+        .join(film_category_df, category_df.category_id == film_category_df.category_id)
+        .join(film_df, film_category_df.film_id == film_df.film_id)
+        .join(inventory_df, film_df.film_id == inventory_df.film_id)
+        .join(rental_df, inventory_df.inventory_id == rental_df.inventory_id)
+        .join(customer_df, rental_df.customer_id == customer_df.customer_id)
+        .join(address_df, customer_df.address_id == address_df.address_id)
+        .join(city_df, address_df.city_id == city_df.city_id)
     )
     
-    rent_a_top_df = category_rent_df.orderBy(col("rent_a").desc()).limit(1) \
-    .select(col("name").alias("category_name"), col("rent_a").alias("rent"))
+    category_rent_df = (
+        joined_df
+        .groupBy(category_df.name) 
+        .agg(
+        sum(when(city_df.city.like("a%"), film_df.rental_duration).otherwise(0)).alias("rent_a"),
+        sum(when(city_df.city.like("%-%"), film_df.rental_duration).otherwise(0)).alias("rent_")
+        )
+    )
+    
+    rent_a_top_df = (
+        category_rent_df
+        .orderBy(col("rent_a").desc())
+        .limit(1) 
+        .select(col("name").alias("category_name"), col("rent_a").alias("rent"))
+    )
+    
 
-    rent__top_df = category_rent_df.orderBy(col("rent_").desc()).limit(1) \
+    rent__top_df = (
+        category_rent_df
+        .orderBy(col("rent_").desc())
+        .limit(1)
         .select(col("name").alias("category_name"), col("rent_").alias("rent"))
-
+    )
+    
     result_df = rent_a_top_df.unionAll(rent__top_df)
     
     return result_df
